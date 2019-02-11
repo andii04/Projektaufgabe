73716e387db519b8ad6b0e63620d817b6be9b432
Schurk, Fabian <fabian.schurk@web.de>
2019-02-11 15:04:27 +0100

    fasfasf

--- src/Parser.java
+++ src/Parser.java
@@ -1,11 +1,13 @@
 public class Parser {
 
     private String regEx;
+    public int pos=0;
 
 private Visitable start(){
     if(regEx== "#") return new OperandNode("#");
     else if(regEx.charAt(0)=='('&& regEx.charAt(regEx.length())=='#'&& regEx.charAt(regEx.length()-1)==')'){
     OperandNode leaf = new OperandNode("#");
+    pos++;
     return new BinOpNode("", regExp(null), leaf);
     }
     else {
@@ -13,34 +15,64 @@ private Visitable start(){
     }
 }
 
-private Visitable  regExp(Visitable v){
+private Visitable  regExp(Visitable p){
     return rE(term(null));
 }
 
-private Visitable rE(Visitable v){
+private Visitable rE(Visitable p){
 }
 
-private Visitable term(Visitable v){
+private Visitable term(Visitable p){
+    char c = regEx.charAt(pos);
 
-}
-private Visitable factor(Visitable v){
-}
-private Visitable hop(Visitable v){
-    switch (){
-        case '?':
-            return  new UnaryOpNode('?', HOp.parameter);
-            break;
-        case '+':
-            return new UnaryOpNode('+', HOp.parameter);
+    if(c>='0'&& c<='9'||c>='a'&& c<='z'||c>='A'&& c<='Z'|| c == '(' ){
+        factor(null);
 
+        if (p != null)
+        {
+            return term(new BinOpNode('',
+                    p, factor(null));)
+        }
+        else
+        {
+            return term(factor(null));
+        }
     }
+    else if (c == '|'|| c == ')'){
+        return p;
+    }
+    return null;
 }
-private Visitable elem(Visitable v){
+private Visitable factor(Visitable p){
+    char c = regEx.charAt(pos);
+    if(c>='0'&& c<='9'||c>='a'&& c<='z'||c>='A'&& c<='Z'|| c == '(' ){
+        return hop(elem(null));
+    }
+    return null;
+}
+private Visitable hop(Visitable p){
+
+    char c = regEx.charAt(pos);
+    if(c>='0'&& c<='9'||c>='a'&& c<='z'||c>='A'&& c<='Z'|| c == '(' ){
+        return p;
+    }
+    else if(c== '*'|| c == '+' || c == '?') return new UnaryOpNode(Character.toString(c), p);
+    return null;
 
 }
+private Visitable elem(Visitable p){
+    char c = regEx.charAt(pos);
+    if(c>='0'&& c<='9'||c>='a'&& c<='z'||c>='A'&& c<='Z'){
+        return alphanum(null);
+    }
+    else if(c == '(') return regExp(null);
 
-private Visitable alphanum(Visitable v){
-
+    return null;
+}
+private Visitable alphanum(Visitable p){
+    char c = regEx.charAt(pos);
+    if (c>='0'&& c<='9'||c>='a'&& c<='z'||c>='A'&& c<='Z') new OperandNode(Character.toString(c));
+    return null;
 }
 }
 
