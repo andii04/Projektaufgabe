4e4a2e79b9a58d7bf64f7cf1687ff261240b2b63
Schurk, Fabian <fabian.schurk@web.de>
2019-02-12 12:27:32 +0100

    fasfasf

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,9 @@
 public class Application {
     public static void main(String[] args) {
-        //visitable visitable=null;
-        //DepthFirstIterator.traverse(visitable,new VisitorOne());
-        //DepthFirstIterator.traverse(visitable,new SecondVisitor());
         Parser p = new Parser();
-        Visitable v = p.go();
-        System.out.println(v);
+        Visitable visitable=p.go();
+        DepthFirstIterator.traverse(visitable,new VisitorOne());
+        DepthFirstIterator.traverse(visitable,new VisitorTwo());
+        System.out.println("fertig");
     }
 }
