7f947735daf07126fc66b9e671b8a78b53316579
Schurk, Fabian <fabian.schurk@web.de>
2019-02-12 12:28:28 +0100

    fasfasf

--- src/Application.java
+++ src/Application.java
@@ -5,5 +5,7 @@ public class Application {
         DepthFirstIterator.traverse(visitable,new VisitorOne());
         DepthFirstIterator.traverse(visitable,new VisitorTwo());
         System.out.println("fertig");
+
+        //https://stackoverflow.com/questions/26948724/create-syntax-tree-from-prograiven-regular-expressions-for-re-to-dfa
     }
 }
